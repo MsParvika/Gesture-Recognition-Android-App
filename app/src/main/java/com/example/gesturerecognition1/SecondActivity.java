e67f707cabf9cf1b6f9cd89e4c2adcae17e7b56c
msparvika <psingha4@asu.edu>
2019-10-05 18:24:21 -0700

    Third Activity(in progress)

--- app/src/main/java/com/example/gesturerecognition1/SecondActivity.java
+++ app/src/main/java/com/example/gesturerecognition1/SecondActivity.java
@@ -1,25 +1,42 @@
 package com.example.gesturerecognition1;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.VideoView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class SecondActivity extends AppCompatActivity {
 
-    String path;
+    String path = "";
     VideoView viedeoPlaceHolder;
     Button practice;
     String gestureName;
+    Intent secondIntent;
+    long startTime = 0;
+    FileOutputStream fo ;
+    File logFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        if(getIntent().hasExtra("gestureName")) {
+        if (getIntent().hasExtra("gestureName")) {
             gestureName = getIntent().getStringExtra("gestureName");
         }
         viedeoPlaceHolder = (VideoView) findViewById(R.id.signVideo);
@@ -28,8 +45,7 @@ public class SecondActivity extends AppCompatActivity {
         MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mediaPlayer) {
-                if(mediaPlayer!=null)
-                {
+                if (mediaPlayer != null) {
                     mediaPlayer.start();
 
                 }
@@ -40,60 +56,139 @@ public class SecondActivity extends AppCompatActivity {
         viedeoPlaceHolder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(!viedeoPlaceHolder.isPlaying()) {
+                if (!viedeoPlaceHolder.isPlaying()) {
                     viedeoPlaceHolder.start();
                 }
             }
         });
+
+        practice.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startCamera();
+                /*secondIntent = new Intent(SecondActivity.this, ThirdActivity.class);
+                secondIntent.putExtra("gestureName", gestureName);
+                startActivity(secondIntent);*/
+
+            }
+        });
+
     }
+
+    public void startCamera() {
+        /*if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            //permission not granted
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+                //doing nothing
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                        101);
+            }
+        }
+
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED) {
+            // Permission not granted
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        100);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+
+        } else {*/
+            // Permission  granted
+            File f = new File(Environment.getExternalStorageDirectory(), "GestureRecognition");
+
+            if (!f.exists()) {
+                f.mkdirs();
+            }
+
+            startTime = System.currentTimeMillis() - startTime;
+
+            Intent t = new Intent(this, ThirdActivity.class);
+            t.putExtra("gestureName", gestureName);
+            t.putExtra("timeElapsed", startTime);
+
+
+            // TODO : Check this part
+           /* final String id = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE).getString(INTENT_ID, "00000000");
+            try {
+                fo = new FileOutputStream(logFile, true);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fo);
+                outputStreamWriter.append("User with ID " + id + " recording video for sign " + gestureName + "\n");
+                outputStreamWriter.close();
+            } catch (IOException e) {
+                Log.e("Exception", "File write failed: " + e.toString());
+            }*/
+
+            // TODO : TIll HERE
+
+            startActivity(t);
+
+       // }
+
+
+    }
+
 
     //  "buy", " house", " fun", " hope", " arrive", " really", " read",
     // " lip", " mouth", " some", " communicate", " write", " create", 
     // " pretend", " sister", " man", " one", " drive", " perfect", " mother"};
 
     public void startVideo(String text) {
-        if(text.equalsIgnoreCase("buy")) {
+        if (text.equalsIgnoreCase("buy")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.buy;
-        } else if(text.equalsIgnoreCase("house")) {
+        } else if (text.equalsIgnoreCase("house")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.house;
         } else if (text.equalsIgnoreCase("fun")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.fun;
-        }else if (text.equalsIgnoreCase("hope")) {
+        } else if (text.equalsIgnoreCase("hope")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.hope;
-        }else if (text.equalsIgnoreCase("arrive")) {
+        } else if (text.equalsIgnoreCase("arrive")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.arrive;
-        }else if (text.equalsIgnoreCase("really")) {
+        } else if (text.equalsIgnoreCase("really")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.really;
-        }else if (text.equalsIgnoreCase("read")) {
+        } else if (text.equalsIgnoreCase("read")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.read;
-        }else if (text.equalsIgnoreCase("lip")) {
+        } else if (text.equalsIgnoreCase("lip")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.lip;
-        }else if (text.equalsIgnoreCase("mouth")) {
+        } else if (text.equalsIgnoreCase("mouth")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.mouth;
-        }else if (text.equalsIgnoreCase("communicate")) {
+        } else if (text.equalsIgnoreCase("communicate")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.comm;
-        }else if (text.equalsIgnoreCase("some")) {
+        } else if (text.equalsIgnoreCase("some")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.some;
-        }else if (text.equalsIgnoreCase("write")) {
+        } else if (text.equalsIgnoreCase("write")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.write;
-        }else if (text.equalsIgnoreCase("create")) {
+        } else if (text.equalsIgnoreCase("create")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.create;
-        }else if (text.equalsIgnoreCase("pretend")) {
+        } else if (text.equalsIgnoreCase("pretend")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.pretend;
-        }else if (text.equalsIgnoreCase("sister")) {
+        } else if (text.equalsIgnoreCase("sister")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.sister;
-        }else if (text.equalsIgnoreCase("man")) {
+        } else if (text.equalsIgnoreCase("man")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.man;
-        }else if (text.equalsIgnoreCase("one")) {
+        } else if (text.equalsIgnoreCase("one")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.one;
-        }else if (text.equalsIgnoreCase("drive")) {
+        } else if (text.equalsIgnoreCase("drive")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.drive;
-        }else if (text.equalsIgnoreCase("perfect")) {
+        } else if (text.equalsIgnoreCase("perfect")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.perfect;
-        }else if (text.equalsIgnoreCase("mother")) {
+        } else if (text.equalsIgnoreCase("mother")) {
             path = "android.resource://" + getPackageName() + "/" + R.raw.mother;
         }
-        if(!path.isEmpty()) {
+        if (!path.isEmpty()) {
             Uri uri = Uri.parse(path);
             viedeoPlaceHolder.setVideoURI(uri);
             viedeoPlaceHolder.start();
--- app/src/main/java/com/example/gesturerecognition1/ThirdActivity.java
+++ app/src/main/java/com/example/gesturerecognition1/ThirdActivity.java
@@ -1,13 +1,140 @@
 package com.example.gesturerecognition1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.ActivityInfo;
+import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class ThirdActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+
+public class ThirdActivity extends AppCompatActivity implements SurfaceHolder.Callback, View.OnClickListener {
+
+    private MediaRecorder recorder;
+    private Camera camera;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private Button mToggleButton;
+    private TextView tv_timer;
+    private TextView tv_time;
+    boolean recording = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        recorder = new MediaRecorder();
+        initRecorder();
         setContentView(R.layout.activity_third);
+
+        surfaceView = (SurfaceView) findViewById(R.id.camera);
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        tv_timer = (TextView) findViewById(R.id.timer);
+        //tv_time = (TextView) findViewById(R.id.tv_time);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        surfaceView.setClickable(true);
+        surfaceView.setOnClickListener(this);
+
+    }
+
+    private void initRecorder() {
+        if(camera == null) {
+            camera = Camera.open(1);
+            camera.setDisplayOrientation(90);
+            try {
+                camera.setPreviewDisplay(surfaceHolder);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            camera.startPreview();
+            camera.unlock();
+        }
+        if(recorder == null)
+            recorder = new MediaRecorder();
+        recorder.setCamera(camera);
+        recorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
+
+        CamcorderProfile cpHigh = CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH);
+        recorder.setProfile(cpHigh);
+
+        // TODO : File name using ID
+/*        File file = new File(Environment.getExternalStorageDirectory().getPath() + "/GestureRecognition/"
+                + sharedPreferences.getString(INTENT_ID,"0000")+"_"+word+"_0_"+format  + ".mp4");
+        //just to be safe
+        while(file.exists()) {
+            i++;
+            file = new File(Environment.getExternalStorageDirectory().getPath() + "/Learn2Sign/"
+                    + sharedPreferences.getString(INTENT_ID,"0000")+"_"+word+"_"+i +"_"+format+ ".mp4");
+        }*/
+        // TODO : Till here
+
+        recorder.setOutputFile("/sdcard/videocapture_example.mp4");
+        recorder.setMaxDuration(5000); // 50 seconds
+        recorder.setMaxFileSize(5000000); // Approximately 5 megabytes
+    }
+
+    public void onClick(View v) {
+        if (recording) {
+            recorder.stop();
+            recording = false;
+
+            // Let's initRecorder so we can record again
+            initRecorder();
+            prepareRecorder();
+        } else {
+            recording = true;
+            recorder.start();
+        }
+    }
+
+    private void prepareRecorder() {
+        recorder.setPreviewDisplay(surfaceHolder.getSurface());
+
+        try {
+            recorder.prepare();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+            finish();
+        } catch (IOException e) {
+            e.printStackTrace();
+            finish();
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        prepareRecorder();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (recording) {
+            recorder.stop();
+            recording = false;
+        }
+        recorder.release();
+        finish();
     }
 }
