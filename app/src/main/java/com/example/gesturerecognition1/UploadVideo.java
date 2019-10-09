877ec84555b15cd006ee569e11d0c22577677555
msparvika <psingha4@asu.edu>
2019-10-08 22:21:26 -0700

    Upload server Working (complete)

--- app/src/main/java/com/example/gesturerecognition1/UploadVideo.java
+++ app/src/main/java/com/example/gesturerecognition1/UploadVideo.java
@@ -1,14 +1,10 @@
 package com.example.gesturerecognition1;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Environment;
 import android.util.Log;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -16,10 +12,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.net.URLConnection;
 
 public class UploadVideo extends AsyncTask<String, Void, Boolean> {
 
@@ -30,106 +28,100 @@ public class UploadVideo extends AsyncTask<String, Void, Boolean> {
 
     @Override
     protected Boolean doInBackground(String... params) {
-        String upLoadServerUri = "http://10.218.107.121/cse535/upload_video.php";
+        //String upLoadServerUri = "http://10.218.107.121/cse535/upload_video.php";
         // String upLoadServerUri = "http://192.168.0.23/upload_video.php";  testing local ip
         // TODO : Parameters
-        String asuId = params[1];
-        String groupId = "123";
-        String urlParameters = "group_id=" + groupId + "&id=" + asuId + "&accept=0";
-        byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
-        // int    postDataLength = postData.length;
-
-        String lineEnd = "\r\n";
-        String twoHyphens = "--";
-        String boundary = "*****";
-        int maxBufferSize = 1 * 1024 * 1024;
-
-        // String [] string = sourceFileUri;
+        String ASUid = params[1];
         String fileName = params[0];
+        File videoFile = new File(fileName);
 
-        HttpURLConnection conn = null;
-        DataOutputStream dataOutputStream = null;
-        //DataInputStream inStream = null;
-        int bytesRead, bytesAvailable, bufferSize;
-        byte[] buffer;
-        String responseFromServer = "";
+        //File videoFile = new File(Environment.getExternalStorageDirectory()+"/my_folder/Action1.mp4");
+        String boundary = Long.toHexString(System.currentTimeMillis()); // Just generate some unique random value.
+        String CRLF = "\r\n"; // Line separator required by multipart/form-data.
 
-        File sourceFile = new File(fileName);
-        if (!sourceFile.isFile()) {
-            Log.e("Huzza", "Source File Does not exist");
-            return false;
-        }
-        try { // open a URL connection to the Servlet
-            URL url = new URL(upLoadServerUri);
+        String url = "http://10.157.78.199/upload_video.php";
 
-            conn = (HttpURLConnection) url.openConnection(); // Open a HTTP  connection to  the URL
-            conn.setDoInput(true); // Allow Inputs
-            conn.setDoOutput(true); // Allow Outputs
-            conn.setUseCaches(false); // Don't use a Cached Copy
-            conn.setRequestMethod("POST");
-            // conn.setRequestProperty( "Content-Length", Integer.toString( postDataLength ));
-            conn.setRequestProperty("Connection", "Keep-Alive");
-            conn.setRequestProperty("ENCTYPE", "multipart/form-data");
-            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-            conn.setRequestProperty("uploaded_file", fileName);
-
-
-            byte[] postDataBytes = postData.toString().getBytes("UTF-8");
-            conn.getOutputStream().write(postDataBytes);
-            dataOutputStream = new DataOutputStream(conn.getOutputStream());
-            dataOutputStream.writeBytes(twoHyphens + boundary + lineEnd);
-            dataOutputStream.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\"" + fileName + "\"" + lineEnd);
-            dataOutputStream.writeBytes(lineEnd);
-
-            FileInputStream fileInputStream = new FileInputStream(sourceFile);
-            bytesAvailable = fileInputStream.available(); // create a buffer of  maximum size
-            Log.i("Huzza", "Initial .available : " + bytesAvailable);
-
-            bufferSize = Math.min(bytesAvailable, maxBufferSize);
-            buffer = new byte[bufferSize];
-
-            // read file and write it into form...
-            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-            while (bytesRead > 0) {
-                try {
-                    dataOutputStream.write(buffer, 0, bufferSize);
-                } catch (OutOfMemoryError e) {
-                    e.printStackTrace();
-                }
-                bytesAvailable = fileInputStream.available();
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-            }
-
-            // send multipart form data necesssary after file data...
-            dataOutputStream.writeBytes(lineEnd);
-            dataOutputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-
-            // Responses from the server (code and message)
-            String serverResponseMessage = conn.getResponseMessage();
-            // close streams
-            Log.i("Upload file to server", "Server Response: " + serverResponseMessage + ", File Name: " + fileName);
-
-            dataOutputStream.flush();
-            dataOutputStream.close();
-            fileInputStream.close();
-        } catch (MalformedURLException ex) {
-            ex.printStackTrace();
-            Log.e("Upload failed", "error: " + ex.getMessage(), ex);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String charset = "UTF-8";
+        String group_id = "40";
+        String accept = "1";
         try {
-            if (conn.getResponseCode() == 200) {
-                return true;
-            } else {
-                return false;
+            URLConnection connection;
+
+            connection = new URL(url).openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+
+            try (
+                    OutputStream output = connection.getOutputStream();
+                    PrintWriter writer = new PrintWriter(new OutputStreamWriter(output, charset), true);
+            ) {
+                // Send normal accept.
+                writer.append("--" + boundary).append(CRLF);
+                writer.append("Content-Disposition: form-data; name=\"accept\"").append(CRLF);
+                writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF);
+                writer.append(CRLF).append(accept).append(CRLF).flush();
+
+                // Send normal accept.
+                writer.append("--" + boundary).append(CRLF);
+                writer.append("Content-Disposition: form-data; name=\"id\"").append(CRLF);
+                writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF);
+                writer.append(CRLF).append(ASUid).append(CRLF).flush();
+
+                // Send normal accept.
+                writer.append("--" + boundary).append(CRLF);
+                writer.append("Content-Disposition: form-data; name=\"group_id\"").append(CRLF);
+                writer.append("Content-Type: text/plain; charset=" + charset).append(CRLF);
+                writer.append(CRLF).append(group_id).append(CRLF).flush();
+
+
+                // Send video file.
+                writer.append("--" + boundary).append(CRLF);
+                writer.append("Content-Disposition: form-data; name=\"uploaded_file\"; filename=\"" + videoFile.getName() + "\"").append(CRLF);
+                writer.append("Content-Type: video/mp4; charset=" + charset).append(CRLF); // Text file itself must be saved in this charset!
+                writer.append(CRLF).flush();
+                FileInputStream vf = new FileInputStream(videoFile);
+                try {
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = 0;
+                    while ((bytesRead = vf.read(buffer, 0, buffer.length)) >= 0) {
+                        output.write(buffer, 0, bytesRead);
+
+                    }
+                    //   output.close();
+                    //Toast.makeText(getApplicationContext(),"Read Done", Toast.LENGTH_LONG).show();
+                } catch (Exception exception) {
+
+
+                    //Toast.makeText(getApplicationContext(),"output exception in catch....."+ exception + "", Toast.LENGTH_LONG).show();
+                    Log.d("Error", String.valueOf(exception));
+                    //publishProgress(String.valueOf(exception));
+                    // output.close();
+
+                }
+
+                output.flush(); // Important before continuing with writer!
+                writer.append(CRLF).flush(); // CRLF is important! It indicates end of boundary.
+
+
+                // End of multipart/form-data.
+                writer.append("--" + boundary + "--").append(CRLF).flush();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
+
+            // Request is lazily fired whenever you need to obtain information about response.
+            int responseCode = ((HttpURLConnection) connection).getResponseCode();
+            System.out.println(responseCode); // Should be 200
+
+            return null;
+        } catch (
+                IOException e) {
             e.printStackTrace();
         }
-        return false;
+
+        return null;
     }
 
     int serverResponseCode;
